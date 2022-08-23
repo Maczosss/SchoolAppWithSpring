4ea34abd6ba85801f7382dfb5c3bf8d11bef4107
Maciej Jaremowicz <maciej.jaremowicz@gmail.com>
2022-08-23 13:03:46 +0200

    test implementing aoc for logging actions performed by app

--- src/main/java/com/example/school/controller/LoginController.java
+++ src/main/java/com/example/school/controller/LoginController.java
@@ -35,7 +35,7 @@ public class LoginController {
         return "login.html";
     }
 
-    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    @RequestMapping(value = "/logout", method = RequestMethod.POST)
     public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
 
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
