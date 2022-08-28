f5b83d6bc89cb578491bb6597e628f1f7379875b
Maciej Jaremowicz <maciej.jaremowicz@gmail.com>
2022-08-28 16:40:25 +0200

    replacing database repositories with jpa interfaces CrudRepository

--- src/main/java/com/example/school/repository/ContactRepository.java
+++ src/main/java/com/example/school/repository/ContactRepository.java
@@ -1,60 +1,15 @@
 package com.example.school.repository;
 
+import com.example.school.constants.SchoolConstants;
 import com.example.school.model.Contact;
-import com.example.school.rowmapper.ContactRowMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementSetter;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
+
 import java.util.List;
 
 @Repository
-public class ContactRepository {
+public interface ContactRepository extends CrudRepository<Contact, Integer> {
 
-    private final JdbcTemplate jdbcTemplate;
+    List<Contact> findByStatus(String status);
 
-    @Autowired
-    public ContactRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    public int saveContactMsg(Contact contact) {
-        String sql = "insert into contact_msg (name,mobile_num,email,subject,message,status," +
-                "created_at,created_by) values (?,?,?,?,?,?,?,?)";
-        return jdbcTemplate.update(sql,
-                contact.getName(),
-                contact.getMobileNum(),
-                contact.getEmail(),
-                contact.getSubject(),
-                contact.getMessage(),
-                contact.getStatus(),
-                contact.getCreatedAt(),
-                contact.getCreatedBy());
-    }
-
-    public List<Contact> findMsgsWithStatus(String status) {
-        String sql = "select * from contact_msg where status = ?";
-        return jdbcTemplate.query(sql, new PreparedStatementSetter() {
-            public void setValues(PreparedStatement preparedStatement) throws SQLException {
-                preparedStatement.setString(1, status);
-            }
-        }, new ContactRowMapper());
-    }
-
-    public int updateMsgStatus(int contactId, String closedStatus, String updatedBy) {
-        var sql = "update contact_msg set status = ?, updated_by = ?, updated_at = ? where contact_id = ?";
-        return jdbcTemplate.update(sql, new PreparedStatementSetter() {
-            @Override
-            public void setValues(PreparedStatement ps) throws SQLException {
-                ps.setString(1, closedStatus);
-                ps.setString(2, updatedBy);
-                ps.setTimestamp(3, Timestamp.valueOf(LocalDateTime.now()));
-                ps.setInt(4, contactId);
-            }
-        });
-    }
 }
--- src/main/java/com/example/school/service/ContactService.java
+++ src/main/java/com/example/school/service/ContactService.java
@@ -6,8 +6,10 @@ import com.example.school.repository.ContactRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -21,26 +23,31 @@ public class ContactService {
     }
 
     public boolean saveMessageDetails(Contact contact) {
-        boolean isSaved = false;
+        var isSaved = false;
         contact.setStatus(SchoolConstants.OPEN);
         contact.setCreatedAt(LocalDateTime.now());
         contact.setCreatedBy(SchoolConstants.ANONYMOUS);
-        int result = contactRepository.saveContactMsg(contact);
-        if (result > 0) {
+        var savedContact = contactRepository.save(contact);
+        if (savedContact.getContactId() > 0) {
             isSaved = true;
         }
         return isSaved;
     }
 
     public List<Contact> findMsgsWithOpenStatus() {
-        var contactMsgs = contactRepository.findMsgsWithStatus(SchoolConstants.OPEN);
-        return contactMsgs;
+        return contactRepository.findByStatus(SchoolConstants.OPEN);
     }
 
     public boolean updateMsgStatus(int contactId, String updatedBy) {
-        boolean isUpdated = false;
-        int result = contactRepository.updateMsgStatus(contactId, SchoolConstants.CLOSE, updatedBy);
-        if (result > 0) {
+        var isUpdated = false;
+        Optional<Contact> contact = contactRepository.findById(contactId);
+        contact.ifPresent(contact1 -> {
+            contact1.setStatus(SchoolConstants.CLOSE);
+            contact1.setUpdatedBy(updatedBy);
+            contact1.setUpdatedAt(LocalDateTime.now());
+        });
+        var updatedContact = contactRepository.save(contact.get());
+        if (updatedContact.getUpdatedBy() != null) {
             isUpdated = true;
         }
         return isUpdated;
