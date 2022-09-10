8877ee3640f84c0e9c704da63670dbd59c8c9253
Maciej Jaremowicz <maciej.jaremowicz@gmail.com>
2022-09-10 15:15:37 +0200

    added delete message ability

--- src/main/java/com/example/school/rest/ContactRestController.java
+++ src/main/java/com/example/school/rest/ContactRestController.java
@@ -1,16 +1,21 @@
 package com.example.school.rest;
 
 import com.example.school.model.Contact;
+import com.example.school.model.Response;
 import com.example.school.repository.ContactRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
-@Controller
+@RestController
 @RequestMapping(path = "/api/contact")
 public class ContactRestController {
 
@@ -18,18 +23,50 @@ public class ContactRestController {
     private ContactRepository contactRepository;
 
     @GetMapping("/getMessagesByStatus")
-    @ResponseBody
+//    @ResponseBody
     public List<Contact> getMessagesByStatus(@RequestParam(name = "status") String status) {
         return contactRepository.findByStatus(status);
     }
 
     @GetMapping("/getAllMsgsByStatus")
-    @ResponseBody
-    public List<Contact> getAllMsgsByStatus(@RequestBody Contact contact){
-        if(contact!=null && contact.getStatus()!=null){
+//    @ResponseBody when using rest controller it's not needed
+    public List<Contact> getAllMsgsByStatus(@RequestBody Contact contact) {
+        if (contact != null && contact.getStatus() != null) {
             return contactRepository.findByStatus(contact.getStatus());
-        }else{
+        } else {
             return List.of();
         }
+    }
+
+    @PostMapping("/saveMsg")
+    public ResponseEntity<Response> saveMsg(@RequestHeader("invocationFrom") String invocationFrom,
+                                            @Valid @RequestBody Contact contact) {
+        log.info(String.format("Header invocationFrom = %s", invocationFrom));
+        contactRepository.save(contact);
+        var response = new Response();
+        response.setStatusCode("200");
+        response.setStatusMsg("Message saved successfully");
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("isMsgSaved", "true")
+                .body(response);
+    }
+
+    @DeleteMapping("/deleteMsg")
+    public ResponseEntity<Response> deleteMsg(RequestEntity<Contact> requestEntity){
+        var headers = requestEntity.getHeaders();
+        headers.forEach((key, value) ->{
+            log.info(String.format(
+                    "Header: '%s' = '%s'", key, value.stream().collect(Collectors.joining("|"))
+            ));
+        });
+        var contact = requestEntity.getBody();
+        contactRepository.deleteById(contact.getContactId());
+        var response = new Response();
+        response.setStatusCode("200");
+        response.setStatusMsg("Message successfully deleted");
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response);
     }
 }
