c77299d98505e3071ecd18d429debf834fb7fe64
Maciej Jaremowicz <maciej.jaremowicz@gmail.com>
2022-09-11 14:26:42 +0200

    adding cros option for allowing all domain acces, and adding dependency for creating response in xml format

--- src/main/java/com/example/school/rest/ContactRestController.java
+++ src/main/java/com/example/school/rest/ContactRestController.java
@@ -7,6 +7,7 @@ import com.example.school.repository.ContactRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +18,8 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
-@RequestMapping(path = "/api/contact")
+@RequestMapping(path = "/api/contact", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+@CrossOrigin(origins = "*")
 public class ContactRestController {
 
     @Autowired
